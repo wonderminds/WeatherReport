2c4cc0816566df4b79dab906752a3571b3bccbdb
wonderminds <manish_madan1988@yahoo.com>
2021-06-15 17:31:38 +0530

    Create Weatherpage.py

---
+++ PageObjects/Weatherpage.py
@@ -0,0 +1,16 @@
+application_title=  "//*[@name='twc-logo']"
+application_url ="https://weather.com/en-IN/"
+Search_city =  "//input[@id='LocationSearch_input']"
+temperature_from_UI=  "//span[@class='TodayDetailsCard--feelsLikeTempValue--2icPt']"
+current_weather_from_UI= "(//*[@data-testid='TemperatureValue'])[1]"
+current_humidity_from_UI= "(//*[@data-testid='PercentageValue'])[1]"
+
+
+
+
+
+
+
+
+
+
