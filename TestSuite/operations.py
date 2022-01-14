21263b6c23450f07e18c196683b5eac92881a492
Manish Madan <manish_madan1988@yahoo.com>
2022-01-14 19:32:28 +0530

    SDS-40:Added proper comments

--- TestSuite/operations.py
+++ TestSuite/operations.py
@@ -1,4 +1,4 @@
-#defining a generic function for subtraction
+#Defining a small function here.
 def subtraction(var a, var b):
     a= int(input('Enter a number')
     b= int(input('Enter another number')
