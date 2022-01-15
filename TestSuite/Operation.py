16dda7531b17dad4ac38c3e4b0be3221fab469bd
Manish Madan <manish_madan1988@yahoo.com>
2022-01-15 13:16:58 +0530

    SDS-40:File Modification

--- TestSuite/Operation.py
+++ TestSuite/Operation.py
@@ -3,3 +3,9 @@ def subtraction():
    var a= int(input('Enter a number')
    var b= int(input('Enter another number')
    print(a-b)
+
+#definging a function for addition
+def additon():
+   var x= int(input('Enter the first number')
+   var y= int(input('Enter the second number')
+   print(a+b)
